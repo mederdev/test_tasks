6bb8e9638b2db0c5a9283998ee872e00ec132108
mederdev <matkalykov.m@icloud.com>
2022-10-11 12:13:01 +0600

    T

--- test3.ts
+++ test3.ts
@@ -2,27 +2,14 @@
 //start time 18:22
 //end time 18:38
 
+
+
 const fibonacciCombination = (target) => {
-	let currenItem = 1;
-	while (true) {
-		const item = fibonacci(currenItem) * fibonacci(currenItem + 1);
-		if (item == target) {
-			return [fibonacci(currenItem), fibonacci(currenItem + 1), true];
-		}
-		if (item > target) {
-			return [fibonacci(currenItem), fibonacci(currenItem + 1), false];
-		}
-		currenItem = currenItem + 1;
+	let paras = [0, 1];
+	while ((paras[0] * paras[1]) < target) {
+		paras = [paras[1], paras[0] + paras[1]];
 	}
+	return [paras[0], paras[1], paras[0] * paras[1] == target];
 }
 
-const fibonacci = (value) => {
-	if (value <= 1) {
-		return value;
-	}
-	else {
-		return fibonacci(value - 1) + fibonacci(value - 2);
-	}
-}
-
-console.log(fibonacciCombination(714));
+console.log(fibonacciCombination(800));
